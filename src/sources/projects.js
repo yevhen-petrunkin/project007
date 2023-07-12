b2f8fbf8352484314f499b32ffd8115fb39b4196
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-07-12 23:45:22 +0300

    Update technologies and projects

--- src/sources/projects.js
+++ src/sources/projects.js
@@ -1,10 +1,11 @@
 const projects = [
-  "V7Pro Bot",
-  "Cinemagot",
-  "Wallet",
-  "Phonebooth",
-  "Filmoteka",
-  "Icecream With Passion",
+  'Promptadise',
+  'V7Pro Bot',
+  'Cinemagot',
+  'Wallet',
+  'Phonebooth',
+  'Filmoteka',
+  'Icecream With Passion',
 ];
 
 export default projects;
